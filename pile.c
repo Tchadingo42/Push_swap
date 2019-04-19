3ba11afe1eae4d6fa43f573b3fca5f8741b24e2f
chbelan <chbelan@42.FR>
2019-04-19 18:50:03 +0200

    dd

---
+++ pile.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pile.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chbelan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/19 16:15:50 by chbelan           #+#    #+#             */
+/*   Updated: 2019/04/19 18:46:49 by chbelan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define pile_donnee(stack) stack->start->data
+#include "pile.h"
+/* set t_pile var */
+void		init_pile(t_pile *stack)
+{
+	stack->size = 0;
+	stack->start = NULL;
+}
+
+int		push_to_stack(t_pile *stack, char *data)
+{
+	t_list	*new_elem;
+
+	if ((new_elem =(t_list*)malloc(sizeof(t_list))) == NULL)
+		return (-1);
+	if ((new_elem->data =(char *)malloc(27 * sizeof(char))) == NULL)
+		return (-1);
+	strcpy(new_elem->data, data);
+	new_elem->next = stack->start;
+	stack->start = new_elem;
+	stack->size++;
+	return (1);
+}
+
+int			destack(t_pile *stack)
+{
+	t_list	*destroy_elem;
+
+	if (stack->size == 0)
+		return (-1);
+	destroy_elem = stack->start;
+	stack->start = stack->start->next;
+	free(destroy_elem->data);
+	free(destroy_elem);
+	stack->size--;
+	return (1);
+}
+
+void		print_pile(t_pile *stack)
+{
+	t_list	*current;
+	int		i;
+
+	current = stack->start;
+	i = 0;
+	while (i < stack->size)
+	{
+		printf("%s\n", current->data);
+		current = current->next;
+		i++;
+	}
+}
+
+int		main(void)
+{
+	t_pile	*stack;
+	char	*name;
+	char	tmp[27];
+
+	if ((stack = (t_pile *)malloc(sizeof(t_pile))) == NULL)
+		return (-1);
+	if ((name = (char *)malloc(27 *sizeof(char))) == NULL)
+		return (-1);
+	init_pile(stack);
+	printf("enter a word ");
+	scanf ("%s", name); 
+	push_to_stack (stack, name);
+    printf ("La pile (%d éléments): \n",stack->size);
+    printf("\n********** Haut de la PILE **********\n");
+    print_pile(stack); printf("__________ Bas de la PILE __________\n\n");
+    printf ("Entrez un mot : ");
+    scanf ("%s", name);
+	push_to_stack (stack, name);
+    printf ("La pile (%d éléments): \n",stack->size);
+    printf("\n********** Haut de la PILE **********\n");
+	print_pile(stack); printf("__________ Bas de la PILE __________\n\n");
+    printf ("Entrez un mot : ");
+    scanf ("%s", name); 
+	push_to_stack (stack, name);
+    printf ("La pile (%d éléments): \n",stack->size);
+    printf("\n********** Haut de la PILE **********\n");
+    print_pile(stack);
+    printf("__________ Bas de la PILE __________\n\n");
+    printf ("\nLe dernier entré (LastInFirstOut) [ %s ] sera supprimé", pile_donnee(stack));
+    printf ("\nLe dernier entré est supprime\n");
+    destack (stack); /* suppression de dernier element entre */ printf ("La pile (%d éléments): \n",stack->size);
+    printf("\n********** Haut de la PILE **********\n"); 
+	print_pile(stack); 
+	printf("__________ Bas de la PILE __________\n\n");
+    return (0);
+}
---
+++ pile.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pile.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chbelan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/19 16:15:58 by chbelan           #+#    #+#             */
+/*   Updated: 2019/04/19 17:50:56 by chbelan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PILE_H
+#define PILE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef	struct		s_list
+{
+	char			*data;
+	struct	s_list	*next;
+}					t_list;
+
+typedef	struct		s_pile
+{
+	int				size;
+	struct	s_list	*start;
+}					t_pile;
+
+void        init_pile(t_pile *stack);
+char        *ft_strpy(char *dest, char *src);
+int         push_to_stack(t_pile *stack, char *data);
+int         destack(t_pile *stack);
+void        print_pile(t_pile *stack);
+
+#endif
